44d6aa221cd15c8de1ea5dabff2f591cee823ff1
Aoitoridayo <s2071150@s.konan-u.ac.jp>
2023-02-09 13:03:29 +0900

    Initial Commit

---
+++ NameListApp/NameListAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NameListAppApp.swift
+//  NameListApp
+//
+//  Created by 渡邊魁優 on 2023/02/09.
+//
+
+import SwiftUI
+
+@main
+struct NameListAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
